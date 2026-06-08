5ac2a6ef6d79a88fd87e98ae49948824bcb7e771
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-08 14:39:53 +0600

    update code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,5 @@
 import App from "@/App";
-import AdminLayout from "@/components/layout/AdminLayout";
 import About from "@/pages/About";
-import Analytics from "@/pages/Analytics";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -12,16 +10,6 @@ export const router = createBrowserRouter([
       {
         Component: About,
         path: 'about'
-      }
-    ]
-  },
-  {
-    Component: AdminLayout,
-    path: '/admin',
-    children: [
-      {
-        Component: Analytics,
-        path: 'analytics'
       }
     ]
   }
