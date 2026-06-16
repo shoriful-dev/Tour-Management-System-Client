fe4be085fce982c0014fe3d0baf4419829d5cfd0
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-16 22:26:14 +0600

    feat: implement axios base query and set up base API for Redux integration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { baseApi } from './baseApi';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
