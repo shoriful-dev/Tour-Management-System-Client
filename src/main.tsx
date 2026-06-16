4e2037ce892575f428ea229a3b665d9c35147281
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-16 22:11:11 +0600

    feat: set up Redux store and authentication API with registration and login mutations

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router';
 import { router } from './routes/index.tsx';
 import { ThemeProvider } from './providers/theme-provider.tsx';
 import { Provider as ReduxProvider } from 'react-redux';
-import { store } from './components/redux/store.ts';
+import { store } from './redux/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
