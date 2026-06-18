409b3d950728f5da3cafd3423fba72952cc75bc4
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-18 22:51:16 +0600

    feat: add Verify component for email verification functionality

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import App from "@/App";
 import About from "@/pages/About";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import Verify from "@/pages/Verify";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -24,7 +25,7 @@ export const router = createBrowserRouter([
     path: '/register',
   },
   {
-    // Component: Verify,
+    Component: Verify,
     path: '/verify',
   },
 ]);
