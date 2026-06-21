8f8423adcf55f8bd5c644710c03c52caeba3a80f
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-21 21:54:30 +0600

    fix: remove unnecessary comments in response interceptor of axios instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -19,8 +19,6 @@ axiosInstance.interceptors.request.use(
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
   function onFulfilled(response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     console.log('Axios', response);
     return response;
   },
