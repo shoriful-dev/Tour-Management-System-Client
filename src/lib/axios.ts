9a5ec19939060364a9b9b94251ae20ce326ff39c
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-21 21:54:05 +0600

    fix: remove unnecessary comment in request interceptor of axios instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -8,7 +8,6 @@ export const axiosInstance = axios.create({
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
   function (config) {
-    // Do something before request is sent
     console.log('Axios', config);
     return config;
   },
