1703bebbb58d092a905cce926ff39994e07aee58
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-29 22:41:30 +0600

    fix: standardize quotes in App component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Outlet } from "react-router";
-import CommonLayout from "./components/layout/CommonLayout";
+import { Outlet } from 'react-router';
+import CommonLayout from './components/layout/CommonLayout';
 
 const App = () => {
   return (
@@ -8,7 +8,7 @@ const App = () => {
         <Outlet />
       </CommonLayout>
     </>
-  )
-}
+  );
+};
 
 export default App;
