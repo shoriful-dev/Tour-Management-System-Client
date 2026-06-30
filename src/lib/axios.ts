eea61c64a88e0aa2e6183cde703af2ba30f0e288
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-30 21:15:34 +0600

    fix: remove redundant comments in axios response interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -22,14 +22,10 @@ axiosInstance.interceptors.request.use(
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
   function onFulfilled(response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     console.log('Axios', response);
     return response;
   },
   function onRejected(error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
     return Promise.reject(error);
   },
 );
