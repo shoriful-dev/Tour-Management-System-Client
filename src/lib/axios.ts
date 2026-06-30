3433daf9ace7d8372ad7674504257b95fff5479f
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-30 21:13:24 +0600

    fix: correct comments for request and response interceptors in axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,24 +6,30 @@ export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-// Add a request interceptors
+// Add a request interceptor
 axiosInstance.interceptors.request.use(
   function (config) {
+    // Do something before request is sent
     console.log('Axios', config);
     return config;
   },
   function (error) {
+    // Do something with request error
     return Promise.reject(error);
   },
 );
 
-// Add a response interceptors
+// Add a response interceptor
 axiosInstance.interceptors.response.use(
   function onFulfilled(response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
     console.log('Axios', response);
     return response;
   },
   function onRejected(error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
     return Promise.reject(error);
   },
-)
+);
