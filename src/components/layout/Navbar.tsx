ab740a08938928693fc96ea5290d468d2d240c5d
shoriful-dev <cmshohelrana50@gmail.com>
2026-07-02 19:59:58 +0600

    fix: adjust SVG path translation for improved animation effect

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -60,7 +60,7 @@ export default function Navbar() {
                   />
                   <path
                     d="M4 12H20"
-                    className="origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+                    className="origin-center translate-y-1.75 transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-135"
                   />
                 </svg>
               </Button>
