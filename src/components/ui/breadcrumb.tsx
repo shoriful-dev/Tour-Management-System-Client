3d766db95a81fa337d963e567ba054cf9c75d922
MD Shoriful Islam <cmshohelrana50@gmail.com>
2026-09-07 19:54:25 +0600

    Fix import statement for Slot from radix-ui

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Slot } from "radix-ui"
+import { Slot } from "radix-ui";
 
 import { cn } from "@/lib/utils"
 import { ChevronRightIcon, MoreHorizontalIcon } from "lucide-react"
