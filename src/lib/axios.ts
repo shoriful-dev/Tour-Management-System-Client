60c596a0281be0acc8882535ed80f54c2e410a3a
MD Shoriful Islam <cmshohelrana50@gmail.com>
2026-09-23 21:13:53 +0600

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,7 +6,6 @@ export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-// Add a request interceptor
 axiosInstance.interceptors.request.use(
   function (config) {
     // Do something before request is sent
